16355d3a2e9afbd41405c836a42cdcb564e484be
Gilles Platteeuw <platteeuw.gilles@gmail.com>
2021-07-08 14:47:53 +0200

    add t1cservice

---
+++ t1c-app-rmc/src/services/Trust1ConnectorService.js
@@ -0,0 +1,42 @@
+import { T1CClient, T1CConfig, T1CConfigOptions } from "t1c-sdk-js";
+
+let client = null;
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+export default {
+  init() {
+    // TODO make the variables configurable via docker
+    // Return a initialised T1C client
+    if (client != null) return client;
+    else {
+      return T1CClient.initialize(
+        new T1CConfig(
+          new T1CConfigOptions(
+            "https://t1c.t1t.io",
+            51883,
+            null,
+            null,
+            null,
+            location.hostname
+          )
+        )
+      );
+    }
+  },
+  getClient() {
+    return client;
+  },
+  setClient(c) {
+    client = c;
+  },
+};
--- t1c-app-rmc/src/views/Home.vue
+++ t1c-app-rmc/src/views/Home.vue
@@ -9,11 +9,27 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { onMounted } from "vue";
+import Trust1ConnectorService from "../services/Trust1ConnectorService";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  setup() {
+    // mounted
+    onMounted(() => {
+      console.log("Component is mounted!");
+      Trust1ConnectorService.init().then(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    });
   },
 };
 </script>
