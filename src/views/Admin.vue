c31fee6a61f38c1b1e3ef0522f0b1f648524af03
Gilles Platteeuw <platteeuw.gilles@gmail.com>
2021-07-14 09:34:31 +0200

    styling

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <h1>Admin page</h1>
+  <div class="container">
+    <div class="t1c-logo">
+      <img src="../assets/t1c-logo-full.png" alt="t1c-logo"/>
+    </div>
+
+    <div class="installation-info-container">
+      <div class="installation-info-item">
+        <h3>Trust1Connector version</h3>
+        <p>3.5.0</p>
+        <div class="installation-info-item-icon">
+          
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<style scoped>
+.t1c-logo {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+}
+
+.installation-info-container {
+  margin-top: 50px;
+  width: 100%;
+  height: 500px;
+}
+
+.installation-info-item {
+  width: 250px;
+  height: 200px;
+  border-radius: 15px;
+  border-bottom: 5px solid #E8E8E8;
+  background-color: #F6F6F6;
+  padding: 15px;
+}
+
+.installation-info-item h3 {
+  font-size: 1.3rem;
+  color: #E05512;
+}
+
+.installation-info-item p {
+  font-weight: bold;
+}
+</style>
