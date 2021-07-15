0e1b001dbf332a76b97525d006294ebaa4a01b50
Ahmed Al-Sayed <ahmed@trust1team.com>
2021-07-15 10:56:25 +0200

    create new files

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex";
+import * as ConnectedCards from "./modules/ConnectedCards"
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    ConnectedCards,
+  },
 });
