cfc2ac6581a0e52b62b750f7c0f0b80daaacd8de
Gilles Platteeuw <platteeuw.gilles@gmail.com>
2021-07-27 12:00:20 +0200

    add modules dropdown

--- src/components/nav/Navigation.vue
+++ src/components/nav/Navigation.vue
@@ -11,37 +11,125 @@
 
     <div class="nav-content">
       <div class="list-group-flush">
-        <router-link to="/">
-          <button type="button" class="list-group-item list-group-item-action">
-            Home
+        <div class="nav-item-container">
+          <router-link to="/">
+            <button
+              type="button"
+              class="list-group-item list-group-item-action"
+            >
+              Home
+            </button>
+          </router-link>
+        </div>
+
+        <div class="nav-item-container">
+          <button
+            @click="dropdown()"
+            type="button"
+            class="list-group-item list-group-item-action dropdown-btn"
+          >
+            <span>Modules</span>
+            <i
+              v-bind:class="{ active: dropdown_active }"
+              class="fas fa-sort-down"
+            ></i>
           </button>
-        </router-link>
-        <button type="button" class="list-group-item list-group-item-action">
-          Modules
-        </button>
-        <router-link to="/file-explorer">
-          <button type="button" class="list-group-item list-group-item-action">
-            File explorer
-          </button>
-        </router-link>
-        <router-link to="/admin">
-          <button type="button" class="list-group-item list-group-item-action">
-            Admin panel
-          </button>
-        </router-link>
+          <div class="modules-list" v-if="dropdown_active">
+            <router-link to="/modules/tokens">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                ID/Tokens
+              </button>
+            </router-link>
+            <router-link to="/modules/payment">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                Payment
+              </button>
+            </router-link>
+            <router-link to="/modules/">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                PKI
+              </button>
+            </router-link>
+            <router-link to="/modules/pki">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                Remote loading
+              </button>
+            </router-link>
+            <router-link to="/modules/transport">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                Transport
+              </button>
+            </router-link>
+            <router-link to="/modules/file-exchange">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                File exchange
+              </button>
+            </router-link>
+            <router-link to="/modules/other">
+              <button
+                type="button"
+                class="list-sub-group-item list-group-item-action"
+              >
+                Other
+              </button>
+            </router-link>
+          </div>
+        </div>
+
+        <div class="nav-item-container">
+          <router-link to="/file-explorer">
+            <button
+              type="button"
+              class="list-group-item list-group-item-action"
+            >
+              Certificate manager
+            </button>
+          </router-link>
+        </div>
+
+        <div class="nav-item-container">
+          <router-link to="/admin">
+            <button
+              type="button"
+              class="list-group-item list-group-item-action"
+            >
+              Admin panel
+            </button>
+          </router-link>
+        </div>
       </div>
     </div>
 
     <div class="nav-footer">
       <div class="list-group-flush">
-        <router-link to="/contact">
-          <button
-            type="button"
-            class="list-group-item list-group-item-action contact"
-          >
-            Contact us
-          </button>
-        </router-link>
+        <div class="nav-item-container contact">
+          <router-link to="/contact">
+            <button
+              type="button"
+              class="list-group-item list-group-item-action"
+            >
+              Contact us
+            </button>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -50,14 +138,35 @@
 <script>
 export default {
   name: "Navigation",
-  data: () => ({}),
-  methods: {},
+  data: () => {
+    return {
+      dropdown_active: false,
+    };
+  },
+  methods: {
+    dropdown() {
+      this.dropdown_active = !this.dropdown_active;
+    },
+  },
   props: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.dropdown-btn .fa-sort-down {
+  transition: all 0.1s ease-in;
+}
+
+.dropdown-btn .active {
+  transform: rotate(-180deg) translateY(-6px);
+}
+
+.dropdown-btn {
+  display: flex;
+  justify-content: space-between;
+}
+
 .nav-container {
   width: 300px;
   background-color: #e05512;
@@ -102,17 +211,41 @@ export default {
   text-decoration: none;
 }
 
+.list-sub-group-item {
+  font-weight: 500;
+  padding: 10px 10px 10px 30px;
+  color: white;
+  background-color: #e05512;
+  font-size: 0.9rem;
+  border: none;
+  width: calc(100% - 20px);
+}
+
+.list-sub-group-item:hover {
+  background-color: #d24d0d;
+  color: white;
+}
+
+.list-sub-group-item:focus {
+  background-color: #d24d0d;
+  color: white;
+}
+
+.nav-item-container {
+  border-left: none;
+  border-right: none;
+  border-top: none;
+  border-bottom: 1px solid white;
+  margin: 0 10px 0 10px;
+}
+
 .list-group-item {
   font-weight: bold;
   padding: 15px 10px 15px 10px;
   color: white;
   background-color: #e05512;
-  margin: 0 10px 0 10px;
-  border-left: none;
-  border-right: none;
-  border-top: none;
-  border-bottom: 1px solid white;
-  width: calc(100% - 20px);
+  border: none;
+  width: 100%;
 }
 
 .list-group-item:hover {
