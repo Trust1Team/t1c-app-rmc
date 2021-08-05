466d8bcde44c6c808169a38748cbc36e8d860f2f
Gilles Platteeuw <platteeuw.gilles@gmail.com>
2021-08-05 11:17:58 +0200

    add fr and update json

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,14 @@
 import { createI18n } from "vue-i18n/index";
 import en from "./locales/en.json";
 import nl from "./locales/nl.json";
+import fr from "./locales/fr.json";
 
 export default createI18n({
-    locale: window.VUE_APP_ENV_DEFAULT_LANGUAGE || "en",
-    fallbackLocale: "en",
-    messages: {
-        en: en,
-        nl: nl
-    }
+  locale: window.VUE_APP_ENV_DEFAULT_LANGUAGE || "en",
+  fallbackLocale: "en",
+  messages: {
+    en: en,
+    nl: nl,
+    fr: fr,
+  },
 });
