d4a0b875cada1968df6aaf0412282605d24f3ea3
Ahmed Al-Sayed <ahmed@trust1team.com>
2021-11-04 10:36:00 +0200

    SpanishDNIe

--- src/views/FileExplorer.vue
+++ src/views/FileExplorer.vue
@@ -2,17 +2,31 @@
   <div>
     <h1>{{ $t("fileExplorer.File explorer") }}</h1>
     <div class="col-6 m-auto">
-      <pinpad />
+      <SpanishDNIe :card-info="cardInfo" />
     </div>
   </div>
 </template>
 
 <script>
-import Pinpad from "../components/UIComponents/Pinpad";
+import SpanishDNIe from "../components/modules/espana/SpanishDNIe";
 export default {
   name: "FileExplorer",
   components: {
-    Pinpad,
+    SpanishDNIe,
   },
+  data() {
+    return {
+      cardInfo: {
+        name: "Jhon Doe",
+        sex: "M",
+        nationality: "ESP",
+        birthDate: "01 01 1980",
+        cardNumber: "BAA000972",
+        validUntil: "01 01 2025",
+        dni: "99999999R",
+        secret: "987654"
+      }
+    }
+  }
 };
 </script>
