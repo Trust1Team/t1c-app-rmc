44f14e58c101cba172dd5c46087583c9e31503c4
Joren De Maeyer <demaeyer.joren@gmail.com>
2022-10-03 13:31:22 +0200

    add prettier and improve eslint setup, format on save is integrated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,39 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/base',
+    // Vue 3 Config
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    // Vue 2 Config, Not the desired one, we want to enforce vue 3 usage.
+    // 'plugin:vue/essential',
+    // 'plugin:vue/strongly-recommended',
+    // 'plugin:vue/recommended',
+    'prettier'
+  ],
+
+  parser: 'vue-eslint-parser',
+
   parserOptions: {
-    parser: 'babel-eslint'
+    vueFeatures: {
+      filter: true,
+      interpolationAsNonHTML: false,
+    },
   },
+
   rules: {
-    eqeqeq: 'off',
-    'no-tabs': 'off',
-    'no-new': 'off',
-    'no-unused-expressions': 'off',
-    'space-before-function-paren': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off'
   }
 }
