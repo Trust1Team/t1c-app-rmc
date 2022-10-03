81d520e96ce32ee90f2c1e775b016efd8e1c8896
Joren De Maeyer <demaeyer.joren@gmail.com>
2022-10-03 14:16:01 +0200

    cleaned up useless dependencies (bundle size significant lower)

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = {
   parallel: true,
   configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        Popper: '@popperjs/core'
-      })
-    ]
+    plugins: []
   }
 }
